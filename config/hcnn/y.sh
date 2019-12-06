340d7a79dbdfcf22670e0f13715ac1c57502c871
Niranjhana Narayanan <nniranjhana@ece.ubc.ca>
2019-12-06 12:42:45 -0600

    Update config, add script to generate config auto
    Signed-off-by: Niranjhana Narayanan <nniranjhana@ece.ubc.ca>

--- config/hcnn/y.sh
+++ config/hcnn/y.sh
@@ -1,4 +1,20 @@
-for ((i=1; i<=6; i++))
+mkdir er eb db ob
+
+for ((j=1; j<=6; j++))
 do
-    mv default-"$i"eb.yaml eb/
+    cp default.yaml eb/default-"$j"eb.yaml
+    cp default.yaml er/default-"$j"er.yaml
 done
+
+for ((j=1; j<=6; j++))
+do
+    sed -i 's/bitFlip/Rand/g' er/default-"$j"er.yaml
+    sed -i "s/= 1.0/= 0.$j/g" eb/default-"$j"eb.yaml
+    sed -i "s/= 1.0/= 0.$j/g" er/default-"$j"er.yaml
+done
+
+cp default.yaml db/default-db.yaml
+sed -i 's/errorRate/dynamicInstance/g' db/default-db.yaml
+
+cp default.yaml ob/default-ob.yaml
+sed -i 's/errorRate/oneFaultPerRun/g' ob/default-ob.yaml
